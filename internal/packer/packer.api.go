7ca11bd75327f95fc6ef603a4c573cff154c60ba
David Shainidze <56347073+dsha256@users.noreply.github.com>
2023-12-22 21:07:49 +0400

    rest(sizes) implement sizes api (#8)
    * implement sizes listing api
    
    * implement sizes posting api
    
    * add docs

--- internal/packer/packer.api.go
+++ internal/packer/packer.api.go
@@ -3,11 +3,16 @@ package packer
 import (
 	"context"
 	"errors"
+	"slices"
+
+	"github.com/dsha256/packer-pro/internal/entity"
 )
 
 var (
 	// ErrorInvalidItems ...
 	ErrorInvalidItems = errors.New("items must be a positive number")
+	// ErrDuplicatedSizes ..
+	ErrDuplicatedSizes = errors.New("sizes must be uniq")
 )
 
 // GetPacketsReq ...
@@ -37,6 +42,109 @@ func (packer *Packer) GetPackets(ctx context.Context, items *GetPacketsReq) (*Ge
 		return &response, err
 	}
 	response.Packets = getMinNecessaryPacks(items.Items, ss)
+
+	return &response, nil
+}
+
+// ListSizesResp ...
+type ListSizesResp struct {
+	SortedSizes []int `query:"sorted_sizes"`
+}
+
+//encore:api public method=GET path=/api/v1/packets/sizes
+func (packer *Packer) ListSizes(ctx context.Context) (*ListSizesResp, error) {
+	var response ListSizesResp
+	ss, err := SortedSizes.Items(ctx, sortedSizesKey)
+	if err != nil {
+		return &response, err
+	}
+
+	// If sorted sizes cashing time is expired, sorted sizes will be retrieved from the DB and then cashed again.
+	if len(ss) == 0 {
+		// making sure about cache clean up to avoid duplicated sizes in the cache
+		err = cleanUpSortedSizesCache(ctx)
+		if err != nil {
+			return &response, err
+		}
+
+		err = refreshSortedSizesCacheFromDB(ctx, packer.entity)
+		if err != nil {
+			return &response, err
+		}
+
+		ss, err = SortedSizes.Items(ctx, sortedSizesKey)
+		if err != nil {
+			return &response, err
+		}
+		response.SortedSizes = ss
+
+		return &response, nil
+	}
+
+	response.SortedSizes = ss
+
+	return &response, nil
+}
+
+// PostSizesReq ...
+type PostSizesReq struct {
+	Sizes []int `json:"sizes"`
+}
+
+// Validate validates PostSizesReq.
+func (req *PostSizesReq) Validate() error {
+	weights := make(map[int]int)
+	for _, size := range req.Sizes {
+		if _, exists := weights[size]; exists {
+			return ErrDuplicatedSizes
+		}
+		weights[size]++
+	}
+
+	return nil
+}
+
+// PostSizesResp ...
+type PostSizesResp struct {
+	SortedSizes []int `json:"sorted_sizes"`
+}
+
+//encore:api public method=POST path=/api/v1/packets/sizes
+func (packer *Packer) PostSizes(ctx context.Context, sizes *PostSizesReq) (*PostSizesResp, error) {
+	var response PostSizesResp
+
+	slices.Sort(sizes.Sizes)
+
+	err := cleanUpDBSizes(ctx, packer.entity)
+	if err != nil {
+		return &response, err
+	}
+
+	sizeCreates := make([]*entity.SizeCreate, len(sizes.Sizes))
+	for i, size := range sizes.Sizes {
+		sizeCreates[i] = packer.entity.Size.Create().SetSize(size)
+	}
+	newSizes, err := packer.entity.Size.CreateBulk(sizeCreates...).Save(ctx)
+	if err != nil {
+		return &response, err
+	}
+
+	err = cleanUpSortedSizesCache(ctx)
+	if err != nil {
+		return &response, err
+	}
+
+	var sortedSizes []int
+	for _, size := range newSizes {
+		sortedSizes = append(sortedSizes, size.Size)
+	}
+
+	err = refreshSortedSizesCache(ctx, response.SortedSizes)
+	if err != nil {
+		return &response, err
+	}
+
+	response.SortedSizes = sortedSizes
 
 	return &response, nil
 }
---
+++ internal/packer/packer.helpers.go
@@ -0,0 +1,102 @@
+package packer
+
+import (
+	"context"
+
+	"github.com/dsha256/packer-pro/internal/entity"
+	entitysize "github.com/dsha256/packer-pro/internal/entity/size"
+)
+
+// getMinNecessaryPacks calculates minimum packs quantity for given items based on packs sizes.
+func getMinNecessaryPacks(items int, sortedSizes []int) map[int]int {
+	necessaryPacks := make(map[int]int)
+	lastUsedPackIndex := len(sortedSizes) - 1
+
+	for lastUsedPackIndex > 0 {
+		if items-sortedSizes[lastUsedPackIndex] >= 0 {
+			necessaryPacks[sortedSizes[lastUsedPackIndex]]++
+			items -= sortedSizes[lastUsedPackIndex]
+		} else {
+			if _, exists := necessaryPacks[sortedSizes[lastUsedPackIndex]]; exists {
+				diff := sortedSizes[lastUsedPackIndex] - items
+				isNotCompatibleWithOtherSizes := false
+				for _, size := range sortedSizes[:lastUsedPackIndex] {
+					if size > diff {
+						isNotCompatibleWithOtherSizes = true
+						break
+					}
+				}
+				if isNotCompatibleWithOtherSizes {
+					necessaryPacks[sortedSizes[lastUsedPackIndex]]++
+					items -= sortedSizes[lastUsedPackIndex]
+					break
+				}
+			}
+			lastUsedPackIndex--
+		}
+	}
+
+	if items > 0 {
+		for _, size := range sortedSizes {
+			if size >= items {
+				necessaryPacks[size]++
+				break
+			}
+		}
+	}
+
+	return necessaryPacks
+}
+
+// refreshSortedSizesCacheFromDB retrieves sorted sizes from the db and caches them.
+func refreshSortedSizesCacheFromDB(ctx context.Context, entity *entity.Client) error {
+	err := cleanUpSortedSizesCache(ctx)
+	if err != nil {
+		return err
+	}
+
+	allSizes, err := entity.Size.Query().Select(entitysize.FieldSize).All(ctx)
+	if err != nil {
+		return err
+	}
+
+	for _, size := range allSizes {
+		_, err = SortedSizes.PushRight(ctx, sortedSizesKey, size.Size)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// cleanUpSortedSizesCache cleans up the redis cluster's keyspace dedicated for SortedSizes.
+func cleanUpSortedSizesCache(ctx context.Context) error {
+	_, err := SortedSizes.Delete(ctx, sortedSizesKey)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// refreshSortedSizesCache ...
+func refreshSortedSizesCache(ctx context.Context, sortedSizes []int) error {
+	for _, size := range sortedSizes {
+		_, err := SortedSizes.PushRight(ctx, sortedSizesKey, size)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func cleanUpDBSizes(ctx context.Context, entity *entity.Client) error {
+	_, err := entity.Size.Delete().Where().Exec(ctx)
+	if err == nil {
+		return err
+	}
+
+	return nil
+}
